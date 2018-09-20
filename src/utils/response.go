6c624ea820ed1f7f0c0628a3f82bb6a1ca6d755f
Jeanwang0178 <33227718+jeanwang0178@users.noreply.github.com>
2018-09-20 18:23:36 +0800

    处理返回功能
    Signed-off-by: w <wjian0124@163.com>

--- src/utils/response.go
+++ src/utils/response.go
@@ -4,10 +4,10 @@ package utils
 const SuccessCode = "success"
 
 //SuccessMsg 成功提示信息
-const SuccessMsg = "操作成功"
+const SuccessMsg = "处理成功"
 
 //FailedCode 请求失败
 const FailedCode = "failed"
 
 //FailedMsg 失败提示信息
-const FailedMsg = "操作失败"
+const FailedMsg = "处理异常"
